7fedb108478d2659703c94fb5ae09a622e3ed2ce
aras armani <aras.armani2014@gmail.com>
2018-10-14 13:15:42 +0300

    change app registery

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './src/PromiseComponent';
+import App from './src/fetchComponent';
 
 AppRegistry.registerComponent('myfetching', () => App);
